64886a69f43bfd634c49e6531d3fe7b27f795887
landycodes <landryandrewsk8@gmail.com>
2023-01-12 13:06:37 -0700

    css adjustment for mobile

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,12 +11,9 @@ export default function AboutMe() {
   return (
     <div className="bgColor">
       <div className="aboutme p-3">
-        <p
-          className="text-start contentbg p-2 w-50 rounded-end lh-base m-0"
-          style={style.resize}
-        >
+        <p className="bio text-start contentbg p-2 rounded-end lh-base m-0">
           Hello! My name is Andrew and I am an aspiring web developer located in
-          Tucson, AZ. I started coding about 1 year ago through tutorial
+          Tucson, AZ. I started coding about 2 year ago through tutorial
           websites such as freecodecamp and TwilioQuest. I am also currently
           attending a college bootcamp to further my skills. My goal is to be
           sufficent enough at programming to build my own websites and software
